d94127927fd0622d563997ac10f247b0f641955c
Lucas <lucasbarroreis@gmail.com>
2020-11-09 11:34:54 -0300

    back stack

--- Fragments/app/src/main/java/br/unitins/fragments/activity/MainActivity.java
+++ Fragments/app/src/main/java/br/unitins/fragments/activity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -57,7 +58,9 @@ public class MainActivity extends AppCompatActivity {
                 segundoFragment = new SegundoFragment();
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.frameConteudo,segundoFragment);
+                transaction.addToBackStack(null);
                 transaction.commit();
+
 
 
             }
