5c466c6e8841ba2389150939d34d2778138cfdec
Lucas <lucasbarroreis@gmail.com>
2020-11-23 11:05:27 -0300

    intent - navegador

---
+++ Navegador/app/src/main/java/br/unitins/navegador/Navegador.java
@@ -0,0 +1,14 @@
+package br.unitins.navegador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Navegador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navegador);
+    }
+}
